c385bd3bf64a91f073554ae2c993c4990d0319f2
Bhaviik Parmar <er.bhavik5202@gmail.com>
2025-12-29 19:34:23 +0530

    Hook Complete

--- 06hook-react-lab/src/main.jsx
+++ 06hook-react-lab/src/main.jsx
@@ -1,3 +1,4 @@
+// Hooks
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -15,4 +16,4 @@ createRoot(document.getElementById('root')).render(
     <Hide /><hr />
     <Timer />
   </StrictMode>,
-)
+);
